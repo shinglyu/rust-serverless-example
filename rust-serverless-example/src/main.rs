78554fbc5497a7ac800d10a7ccf3c42e64a45601
Shing Lyu <shing.lyu@gmail.com>
2019-07-25 13:25:02 +0200

    feat: added IAM permission to list tables from dynamodb

--- rust-serverless-example/src/main.rs
+++ rust-serverless-example/src/main.rs
@@ -1,9 +1,15 @@
+//extern crate rusoto_core;
+//extern crate rusoto_dynamodb;
+
 use std::error::Error;
 use lambda_http::{lambda, IntoResponse, Request, RequestExt};
 use lambda_runtime::{error::HandlerError, Context};
 use log::{self, info};
 use simple_logger;
 use serde_json::json;
+
+use rusoto_core::Region;
+use rusoto_dynamodb::{DynamoDb, DynamoDbClient, ListTablesInput};
 
 fn main() -> Result<(), Box<dyn Error>> {
     simple_logger::init_with_level(log::Level::Debug)?;
@@ -18,6 +24,29 @@ fn handler(
 ) -> Result<impl IntoResponse, HandlerError> {
     // `serde_json::Values` impl `IntoResponse` by default
     // creating an application/json response
+    let client = DynamoDbClient::new(Region::UsEast1);
+    let list_tables_input: ListTablesInput = Default::default();
+
+    match client.list_tables(list_tables_input).sync() {
+    Ok(output) => {
+      match output.table_names {
+        Some(table_name_list) => {
+          println!("Tables in database:");
+
+          for table_name in table_name_list {
+            println!("{}", table_name);
+            return Ok(json!({
+                "message": format!("Hello, {}", table_name),
+            }).into_response());
+          }
+        }
+        None => println!("No tables in database!"),
+      }
+    }
+    Err(error) => {
+      println!("Error: {:?}", error);
+    }
+  }
     let response = match req.query_string_parameters().get("first_name") {
         Some(first_name) => json!({
             "message": format!("Hello, {}!", first_name),
