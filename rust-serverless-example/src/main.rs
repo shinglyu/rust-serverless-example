f0708c1183cd6000d2670c8052ac505c77e3760e
Shing Lyu <shing.lyu@gmail.com>
2019-08-05 16:59:41 +0200

    Return 404 and 500 errors

--- rust-serverless-example/src/main.rs
+++ rust-serverless-example/src/main.rs
@@ -1,15 +1,18 @@
 //extern crate rusoto_core;
 //extern crate rusoto_dynamodb;
 
+use std::collections::HashMap;
 use std::error::Error;
 use lambda_http::{lambda, IntoResponse, Request, RequestExt};
+use lambda_http::http::{StatusCode, Response};
 use lambda_runtime::{error::HandlerError, Context};
 use log::{self, info};
 use simple_logger;
 use serde_json::json;
 
-use rusoto_core::Region;
-use rusoto_dynamodb::{DynamoDb, DynamoDbClient, ListTablesInput};
+use rusoto_core::{Region, RusotoError};
+use rusoto_dynamodb::{AttributeValue, DynamoDb, DynamoDbClient,
+    GetItemInput, GetItemOutput, GetItemError, ListTablesInput};
 
 fn main() -> Result<(), Box<dyn Error>> {
     simple_logger::init_with_level(log::Level::Debug)?;
@@ -25,41 +28,59 @@ fn handler(
     // `serde_json::Values` impl `IntoResponse` by default
     // creating an application/json response
     let client = DynamoDbClient::new(Region::UsEast1);
-    let list_tables_input: ListTablesInput = Default::default();
 
-    match client.list_tables(list_tables_input).sync() {
-    Ok(output) => {
-      match output.table_names {
-        Some(table_name_list) => {
-          println!("Tables in database:");
+    let mut key: HashMap<String, AttributeValue> = HashMap::new();
+    key.insert("giftcode".to_string(), AttributeValue { s: Some("ABC124".to_string()), ..Default::default() });
 
-          for table_name in table_name_list {
-            println!("{}", table_name);
-            return Ok(json!({
-                "message": format!("Hello, {}", table_name),
-            }).into_response());
-          }
-        }
-        None => println!("No tables in database!"),
-      }
-    }
-    Err(error) => {
-      println!("Error: {:?}", error);
-    }
-  }
-    let response = match req.query_string_parameters().get("first_name") {
-        Some(first_name) => json!({
-            "message": format!("Hello, {}!", first_name),
-        }).into_response(),
-        None => {
-            info!("No first_name is provided.");
-            json!({
-                "message": "Hello there!"
-            }).into_response()
-        }
+    let get_item_input: GetItemInput = GetItemInput {
+        table_name: "gift-codes".to_string(),
+        key: key,
+        ..Default::default()
+        /*
+        attributes_to_get: None, // deprecated
+        consistent_read: None,
+        expression_attribute_names: None,
+        projection_expression: None, // return everything
+        return_consumed_capacity: None,
+        */
     };
 
-    Ok(response)
+    match client.get_item(get_item_input).sync() {
+        Ok(output) => {
+            println!("{:?}", output);
+            match output.item {
+                Some(item) => {
+                    return Ok(json!({
+                        "giftcode": item.get("giftcode").unwrap().s,
+                        "status": item.get("status").unwrap().s,
+                    }).into_response())
+                }
+                None => {
+                    return Ok(
+                        Response::builder()
+                            .status(StatusCode::NOT_FOUND)
+                            .body("Gift code not found".into())
+                            .expect("Failed to render response")
+                    )
+                }
+            }
+        }
+        Err(error) => {
+            return Ok(
+                Response::builder()
+                .status(StatusCode::INTERNAL_SERVER_ERROR)
+                .body(format!("{:?}", error).into())
+                .expect("Failed to render response")
+            )
+        }
+    }
+
+    return Ok(
+        Response::builder()
+        .status(StatusCode::INTERNAL_SERVER_ERROR)
+        .body("Unhandled error".into())
+        .expect("Failed to render response")
+    )
 }
 
 #[cfg(test)]
